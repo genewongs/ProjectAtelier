bd3fe9ee9b33b9353da4d02d6084b1d1b14dbe4e
Gene Wong <gene.h.wong@gmail.com>
2022-03-31 12:39:12 -0700

    basic functionality of image carousel working.

--- client/src/components/Overview/components/Gallery.jsx
+++ client/src/components/Overview/components/Gallery.jsx
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
-import { GalleryStyled, GalleryInnerStyled } from './styles/GalleryStyled.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  GalleryStyled, GalleryInnerStyled,
+  GalleryInnerLeftStyled, GalleryInnerCenterStyled, GalleryInnerRightStyled,
+} from './styles/GalleryStyled.js';
 
 function Gallery({ styles }) {
   const [img, setImg] = useState(0);
-  console.log(styles.results[0])
-  return(
+
+  return (
     <GalleryStyled>
       <GalleryInnerStyled img={styles.results[0].photos[img].url}>
 
+        <GalleryInnerLeftStyled>
+          <FontAwesomeIcon icon={faCircleChevronLeft} size="2x" onClick={() => { img > 0 && setImg(img - 1); }} />
+        </GalleryInnerLeftStyled>
+
+        <GalleryInnerCenterStyled />
+
+        <GalleryInnerRightStyled>
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            size="2x"
+            onClick={() => {
+              img < styles.results[0].photos.length - 1 && setImg(img + 1);
+            }}
+          />
+        </GalleryInnerRightStyled>
+
       </GalleryInnerStyled>
     </GalleryStyled>
-  )
+  );
 }
 
 export default Gallery;
--- client/src/components/Overview/components/styles/GalleryStyled.js
+++ client/src/components/Overview/components/styles/GalleryStyled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const GalleryStyled = styled.div`
-  width: 50%;
+  width: 60%;
   height: 700px;
   background-color: darkgray;
 `;
@@ -12,5 +12,41 @@ export const GalleryInnerStyled = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  background-image: url(${props => props.img || ''});
+  background-image: url(${(props) => props.img || ''});
+  display: flex;
+`;
+
+export const GalleryInnerLeftStyled = styled.div`
+  display: flex;
+  flex: 10%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.105);
+  transition: all ease-in-out 0.03s;
+  &:hover {
+    cursor: pointer;
+    font-size: 1.1em;
+    transition: all ease-in-out 0.03s;
+  }
+`;
+
+export const GalleryInnerCenterStyled = styled.div`
+  flex: 80%;
+  height: 100%;
+`;
+
+export const GalleryInnerRightStyled = styled.div`
+  display: flex;
+  flex: 10%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.105);
+  transition: all ease-in-out 0.03s;
+  &:hover {
+    cursor: pointer;
+    font-size: 1.1em;
+    transition: all ease-in-out 0.03s;
+  }
 `;
