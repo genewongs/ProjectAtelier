71d72fd601f46cc3e1ca3ae650b64fc0fe1a5db5
DannyWong <danny.96.wong@gmail.com>
2022-03-30 20:46:42 -0700

    updated webpack to include styled components

--- client/src/components/RR/RR.jsx
+++ client/src/components/RR/RR.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function RR() {
   return (
-    <div>Hello we`&apos;`re inside Ratings and Reviews</div>
+    <div>Hello we'&apos;'re inside Ratings and Reviews</div>
   );
 }
