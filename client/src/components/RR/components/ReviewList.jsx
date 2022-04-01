31ec6e9505b4649234be2e34d02fad8fbdc6481f
DannyWong <danny.96.wong@gmail.com>
2022-03-31 19:03:04 -0700

    refactored how reviews are displayed, load messages button disappears if less than 3 messages or all messages are loaded, added an add message button

--- client/src/components/RR/components/ReviewList.jsx
+++ client/src/components/RR/components/ReviewList.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import Review from './Review.jsx';
 import ReviewContext from '../utils/ReviewContext.jsx';
 
-export default function ReviewList({ getMoreReviews }) {
+export default function ReviewList() {
   return (
     <ReviewContext.Consumer>
       {({ reviews }) => (
         <div className="review-list">
           {reviews.map((review) => <Review key={review.review_id} review={review} />)}
-          <button
-            type="button"
-            className="reviews-button"
-            onClick={getMoreReviews}
-          >
-            More Reviews
-          </button>
         </div>
       )}
     </ReviewContext.Consumer>
