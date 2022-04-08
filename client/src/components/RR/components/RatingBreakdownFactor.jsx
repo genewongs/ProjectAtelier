b29afbd22649c25294f664f3ac3dee342c9d6da0
DannyWong <danny.96.wong@gmail.com>
2022-04-08 01:41:37 -0700

    refactor: change breakdownFactors to properly display the correct corresponding highest and lowest values for each characteristic

--- client/src/components/RR/components/RatingBreakdownFactor.jsx
+++ client/src/components/RR/components/RatingBreakdownFactor.jsx
@@ -4,6 +4,49 @@ import StyledRatingBreakdownFactors from './styles/StyledRatingBreakdownFactors'
 
 function RatingBreakdownFactor() {
   const { metaData } = useContext(ReviewStoreContext);
+  const Size = {
+    low: 'A size too small',
+    high: 'A size too wide',
+  };
+  const Width = {
+    low: 'Too narrow',
+    high: 'Too wide',
+  };
+  const Comfort = {
+    low: 'Uncomfortable',
+    high: 'Perfect',
+  };
+  const Quality = {
+    low: 'Poor',
+    high: 'Perfect',
+  };
+  const Length = {
+    low: 'Runs Short',
+    high: 'Runs Long',
+  };
+  const Fit = {
+    low: 'Runs tight',
+    high: 'Runs Long',
+  };
+
+  function returnDescription(key, val) {
+    switch (key) {
+      case 'Size':
+        return Size[val];
+      case 'Width':
+        return Width[val];
+      case 'Comfort':
+        return Comfort[val];
+      case 'Quality':
+        return Quality[val];
+      case 'Length':
+        return Length[val];
+      case 'Fit':
+        return Fit[val];
+      default:
+        return null;
+    }
+  }
 
   return (
     <StyledRatingBreakdownFactors>
@@ -25,9 +68,8 @@ function RatingBreakdownFactor() {
                 value={Math.round(metaData.characteristics[key].value * 10) / 10}
               />
               <div className="description-container">
-                <span className="description-left">Too Small</span>
-                <span className="description-middle">Perfect</span>
-                <span className="description-right">Too Big</span>
+                <span className="description-left">{returnDescription(key, 'low')}</span>
+                <span className="description-right">{returnDescription(key, 'high')}</span>
               </div>
             </div>
           </div>
